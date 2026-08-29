19bc1d71f9788b8a86155ce0f1f8360c1a80a627
FLDebug10 <fld_10@outlook.de>
2026-08-29 20:01:52 +0200

    Implemented the Prevent Bee Anger Power Type similar to Mob Origins Implementation for Fabric 1.21.1

---
+++ src/main/java/dev/overgrown/apoli/power/builtin/PreventBeeAngerPower.java
@@ -0,0 +1,31 @@
+package dev.overgrown.apoli.power.builtin;
+
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import dev.overgrown.apoli.action.BiEntityAction;
+import dev.overgrown.apoli.action.BlockAction;
+import dev.overgrown.apoli.codec.LoggedOptionalField;
+import dev.overgrown.apoli.condition.BiEntityCondition;
+import dev.overgrown.apoli.condition.BlockCondition;
+import dev.overgrown.apoli.power.PowerType;
+
+import java.util.Optional;
+
+public class PreventBeeAngerPower extends PowerType<PreventBeeAngerPower.Config> {
+    public record Config(
+            Optional<BlockCondition> blockCondition,
+            Optional<BlockAction> blockAction,
+            Optional<BiEntityCondition> biEntityCondition,
+            Optional<BiEntityAction> biEntityAction
+    ) {}
+
+    @Override
+    public MapCodec<Config> configCodec() {
+        return RecordCodecBuilder.mapCodec( i -> i.group (
+                LoggedOptionalField.of("block_condition", BlockCondition.CODEC).forGetter(Config::blockCondition),
+                LoggedOptionalField.of("block_action", BlockAction.CODEC).forGetter(Config::blockAction),
+                LoggedOptionalField.of("bientity_condition", BiEntityCondition.CODEC).forGetter(Config::biEntityCondition),
+                LoggedOptionalField.of("bientity_action", BiEntityAction.CODEC).forGetter(Config::biEntityAction)
+        ).apply(i, Config::new));
+    }
+}
