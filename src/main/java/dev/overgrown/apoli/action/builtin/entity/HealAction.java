19de485c517bdb79b3a08a753ab18973f4e227b0
PolarKookie <polarkookie23@gmail.com>
2026-09-23 16:27:09 -0400

    Commit: Fabric 1.20.1
    Heal entity action now can heal into negatives, to deal damage that does not flash the screen or play a sound

--- src/main/java/dev/overgrown/apoli/action/builtin/entity/HealAction.java
+++ src/main/java/dev/overgrown/apoli/action/builtin/entity/HealAction.java
@@ -21,6 +21,10 @@ public final class HealAction implements ActionType<EntityCtx, HealAction.Cfg> {
     public void run(Cfg cfg, EntityCtx ctx) {
         LivingEntity living = ctx.living();
         if (living == null) return;
-        living.heal((float) cfg.amount.eval(living));
+        if (cfg.amount.eval(living) >= 0) {
+            living.heal((float) cfg.amount.eval(living));
+        } else {
+            living.setHealth(living.getHealth() + (float) cfg.amount.eval(living));
+        }
     }
 }
